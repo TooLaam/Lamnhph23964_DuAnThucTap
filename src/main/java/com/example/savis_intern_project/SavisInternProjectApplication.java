a4637214c89bff56ddab1a4744ffa5d1167c05df
LucMinhQuan <quanluc2102@gmail.com>
2023-09-12 11:01:12 +0700

    create new project

---
+++ src/main/java/com/example/savis_intern_project/SavisInternProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.savis_intern_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavisInternProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SavisInternProjectApplication.class, args);
+    }
+
+}
